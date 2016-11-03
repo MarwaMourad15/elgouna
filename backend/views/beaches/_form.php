a1b525ac2d288586a89f965e8763161663b46ad7
Muhammad Fathy Elbermawy <mido.prameg@gmail.com>
2016-11-03 17:09:04 +0200

    Backend - Search

--- backend/views/beaches/_form.php
+++ backend/views/beaches/_form.php
@@ -1,7 +1,8 @@
 <?php
-
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use backend\models\BeachCategory;
+use backend\models\Location;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Beaches */
@@ -12,72 +13,72 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'name')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'type')->textInput(['maxlength' => true])?>
 
-    <?= $form->field($model, 'location')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'location')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'longitude')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'longitude')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'latitude')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'latitude')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'reviewScore')->textInput() ?>
+    <?= $form->field($model, 'reviewScore')->textInput()?>
 
-    <?= $form->field($model, 'ratingStar')->textInput() ?>
+    <?= $form->field($model, 'ratingStar')->textInput()?>
 
-    <?= $form->field($model, 'offerExists')->textInput() ?>
+    <?= $form->field($model, 'offerExists')->textInput()?>
 
-    <?= $form->field($model, 'descrip')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'descrip')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'offerTitle')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'offerTitle')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'offerDescription')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'offerDescription')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'isPoolAvailable')->textInput() ?>
+    <?= $form->field($model, 'isPoolAvailable')->textInput()?>
 
-    <?= $form->field($model, 'isGymAvailable')->textInput() ?>
+    <?= $form->field($model, 'isGymAvailable')->textInput()?>
 
-    <?= $form->field($model, 'isWifiAvailable')->textInput() ?>
+    <?= $form->field($model, 'isWifiAvailable')->textInput()?>
 
-    <?= $form->field($model, 'isVisaPaymentAvailable')->textInput() ?>
+    <?= $form->field($model, 'isVisaPaymentAvailable')->textInput()?>
 
-    <?= $form->field($model, 'isDiningInAvailable')->textInput() ?>
+    <?= $form->field($model, 'isDiningInAvailable')->textInput()?>
 
-    <?= $form->field($model, 'accomadtionType')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'accomadtionType')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'elgounaVoice')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'elgounaVoice')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'email')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'email')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'phoneNumber')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'phoneNumber')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'info')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'info')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'facebookLink')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'facebookLink')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'twitterLink')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'twitterLink')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'instagramLink')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'instagramLink')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'youtubeLink')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'youtubeLink')->textarea(['rows' => 6])?>
 
-    <?= $form->field($model, 'ord')->textInput() ?>
+    <?= $form->field($model, 'ord')->textInput()?>
 
-    <?= $form->field($model, 'hidden')->textInput() ?>
+    <?= $form->field($model, 'hidden')->textInput()?>
 
-    <?= $form->field($model, 'category_id')->textInput() ?>
+    <?= $form->field($model, 'category_id')->dropDownList(ArrayHelper::map( BeachCategory::find()->all(), 'id', 'name' ),[ 'prompt' => '' ])?>
 
-    <?= $form->field($model, 'price_type')->textInput() ?>
+    <?= $form->field($model, 'price_type')->textInput()?>
 
-    <?= $form->field($model, 'popularity')->textInput() ?>
+    <?= $form->field($model, 'popularity')->textInput()?>
 
-    <?= $form->field($model, 'location_id')->textInput() ?>
+    <?= $form->field($model, 'location_id')->dropDownList(ArrayHelper::map( Location::find()->all(), 'id', 'name' ),[ 'prompt' => '' ])?>
 
-    <?= $form->field($model, 'place_type')->textInput() ?>
+    <?= $form->field($model, 'place_type')->textInput()?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary'])?>
     </div>
 
     <?php ActiveForm::end(); ?>
--- backend/views/hotel-review/_search.php
+++ backend/views/hotel-review/_search.php
@@ -1,7 +1,8 @@
 <?php
-
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use backend\models\Users;
+use backend\models\Hotels
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\HotelReviewSearch */
@@ -10,28 +11,33 @@ use yii\widgets\ActiveForm;
 
 <div class="hotel-review-search">
 
-    <?php $form = ActiveForm::begin([
-        'action' => ['index'],
-        'method' => 'get',
-    ]); ?>
+    <?php
+				
+				$form = ActiveForm::begin ( [ 
+						'action' => [ 
+								'index' 
+						],
+						'method' => 'get' 
+				] );
+				?>
 
-    <?= $form->field($model, 'id') ?>
+    <?= $form->field($model, 'id')?>
 
-    <?= $form->field($model, 'hotel_id') ?>
+    <?= $form->field($model, 'hotel_id')->dropDownList(ArrayHelper::map( Hotels::find()->all(), 'id', 'name' ),[ 'prompt' => '' ])?>
 
-    <?= $form->field($model, 'user_id') ?>
+    <?= $form->field($model, 'user_id')->dropDownList(ArrayHelper::map( Users::find()->all(), 'id', 'name' ),[ 'prompt' => '' ])?>
 
-    <?= $form->field($model, 'review') ?>
+    <?= $form->field($model, 'review')?>
 
-    <?= $form->field($model, 'rating') ?>
+    <?= $form->field($model, 'rating')?>
 
     <?php // echo $form->field($model, 'date') ?>
 
     <?php // echo $form->field($model, 'approved') ?>
 
     <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary'])?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default'])?>
     </div>
 
     <?php ActiveForm::end(); ?>
